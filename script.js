524421503cb9d04ad7b89f1014c138af23d0ca2d
Bek Brace <60483846+BekBrace@users.noreply.github.com>
2023-11-30 10:59:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 function searchRecipe() {
     const dishName = document.getElementById('dishName').value;
     const recipeResultElement = document.getElementById('recipeResult');
-    const footer = document.querySelector('footer');
+    
+    const footer = document.querySelector('footer'); // ignore this line
     
     // Using TheMealDB API
+    // Search meal by name - first option in https://www.themealdb.com/api.php
     const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${dishName}`;
   
+    //Fetch sends a HTTP GET request to the specified API endpoint, to the MealDB server,  and fetch  returns a promise that resolves to the response of that HTTP request 
     fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
+    // .then used to handle asynchronous reponse. it takes the response from API and converts it to JSON format. the json() method acutall returns another promise that resolved eventually to the actual data.
+    .then(response => response.json()) 
+      .then(data => {  //callback function processes the data received from API 
         // Check if the meal is found
         if (data.meals) {
           const meal = data.meals[0];
-  
           // Display the recipe details, image, and ingredients
           recipeResultElement.innerHTML = `
             <h2>${meal.strMeal}</h2>
@@ -26,7 +29,7 @@ function searchRecipe() {
           `;
   
           // Update footer content dynamically
-          updateFooter(meal.strMeal, 'Amir | Bek Brace');
+          updateFooter();
   
           // Adjust the layout to accommodate the dynamic content
           adjustLayout();
@@ -34,7 +37,7 @@ function searchRecipe() {
           recipeResultElement.innerHTML = 'Recipe not found. Please try another dish.';
   
           // Reset footer content and layout if no recipe is found
-          updateFooter('', 'Amir | Bek Brace');
+          updateFooter();
           resetLayout();
         }
       })
@@ -43,11 +46,12 @@ function searchRecipe() {
         recipeResultElement.innerHTML = 'Error fetching recipe. Please try again.';
   
         // Reset footer content and layout in case of an error
-        updateFooter('', 'Amir | Bek Brace');
+        updateFooter();
         resetLayout();
       });
   }
   
+  // Ingredients 
   function formatIngredients(meal) {
     const ingredients = [];
     for (let i = 1; i <= 20; i++) {
@@ -62,30 +66,33 @@ function searchRecipe() {
     return ingredients.map(ingredient => `<li>${ingredient}</li>`).join('');
   }
   
-  // Rest of the functions remain the same
+  //  Instructions
   function formatInstructions(instructions) {
     // Convert line breaks to list items
     const steps = instructions.split('\r\n').filter(step => step.trim() !== '');
     return steps.map(step => `<li>${step}</li>`).join('');
   }
   
-  function updateFooter(recipeName, designerName) {
+  // Update footer
+  function updateFooter() {
     const footerContent = document.querySelector('.footer-content');
     const currentYear = new Date().getFullYear();
   
     // Update the content dynamically
     footerContent.innerHTML = `
-      <p>&copy; ${currentYear} ${recipeName ? `${recipeName} Recipe` : 'Recipe Finder'}. All rights reserved.</p>
-      <p>Designed with ❤️ by ${designerName}</p>
+      <p>&copy; ${currentYear} Recipe Finder. All rights reserved.</p>
+      <p>Designed with ❤️ by Amir | Bek Brace</p>
     `;
   }
   
+  // Adjust the layout
   function adjustLayout() {
     const footer = document.querySelector('footer');
     footer.style.position = 'relative';
     footer.style.bottom = 'auto';
   }
   
+  // Reset layout
   function resetLayout() {
     const footer = document.querySelector('footer');
     footer.style.position = 'fixed';
